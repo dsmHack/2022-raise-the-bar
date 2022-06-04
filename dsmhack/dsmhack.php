363b32fe295d671c8ce79c836906aca88a464cce
Ben <bensevcik@gmail.com>
2022-06-04 17:15:06 -0500

    added wordpress plugin

---
+++ dsmhack/dsmhack.php
@@ -0,0 +1,14 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly
+}
+/*
+Plugin Name: DSM Hack 2022
+Plugin URI: dsmhack.org
+description: DSM Hack 2022 Plugin
+Version: 1.0
+Author: DSM Hack
+License: GPL2
+*/
+
